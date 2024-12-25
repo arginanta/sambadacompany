3c00d1b79b71309f6a11f2f7bcf2c17fb975143c
Arginanta <arginanta30@gmail.com>
2024-12-26 02:30:16 +0700

    Mengatur Field dan ORM Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'tagline',
+        'thumbnail',
+        'abaout'
+    ];
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
--- app/Models/Testimonial.php
+++ app/Models/Testimonial.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Testimonial extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'message',
+        'thuhmnail',
+        'project_client_id',
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(ProjectClient::class, 'project_client_id');
+    }
 }
