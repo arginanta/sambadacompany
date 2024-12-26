f5b373f0cb08b68d55361974dd2e7f1e41f65167
Arginanta <arginanta30@gmail.com>
2024-12-26 14:36:28 +0700

    Membuat Laravel Route

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,76 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\CompanyAboutController;
+use App\Http\Controllers\CompanyStatisticController;
+use App\Http\Controllers\HeroSectionController;
+use App\Http\Controllers\OurPrincipleController;
+use App\Http\Controllers\OurTeamController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectClientController;
+use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route untuk halaman dashboard. 
+// Memerlukan middleware 'auth' (autentikasi) dan 'verified' (email diverifikasi). 
+// Route ini juga memiliki nama 'dashboard'.
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Membuat grup route yang membutuhkan autentikasi (middleware 'auth').
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Grup route dengan middleware izin akses khusus:
+    // - Setiap grup hanya dapat diakses oleh user dengan izin tertentu (contoh: 'manage statistics', 'manage products').
+    // - Menggunakan prefix 'admin/' dan nama route 'admin.' untuk menandai sebagai route admin.
+    // - Mengatur fitur CRUD untuk berbagai entitas (statistik, produk, prinsip, testimoni, dll.) melalui controller terkait menggunakan Route::resource().
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::middleware('can:manage statistics')->group(function () {
+            Route::resource('statistic', CompanyStatisticController::class);
+        });
+
+        Route::middleware('can:manage products')->group(function () {
+            Route::resource('products', ProductController::class);
+        });
+
+        Route::middleware('can:manage principles')->group(function () {
+            Route::resource('principles', OurPrincipleController::class);
+        });
+
+        Route::middleware('can:manage testimonials')->group(function () {
+            Route::resource('testimonials', TestimonialController::class);
+        });
+
+        Route::middleware('can:manage clients')->group(function () {
+            Route::resource('clients', ProjectClientController::class);
+        });
+
+        Route::middleware('can:manage teams')->group(function () {
+            Route::resource('teams', OurTeamController::class);
+        });
+
+        Route::middleware('can:manage abouts')->group(function () {
+            Route::resource('abouts', CompanyAboutController::class);
+        });
+
+        Route::middleware('can:manage appointments')->group(function () {
+            Route::resource('appointments', AppointmentController::class);
+        });
+
+        Route::middleware('can:manage hero sections')->group(function () {
+            Route::resource('hero_sections', HeroSectionController::class);
+        });
+    });
 });
 
-require __DIR__.'/auth.php';
+// Menyertakan route untuk autentikasi (login, register, lupa password, dsb.).
+require __DIR__ . '/auth.php';
