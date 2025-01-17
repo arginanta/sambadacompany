bd889cf4d55779922b7bbf861063ead849a6a114
Arginanta <arginanta30@gmail.com>
2025-01-17 13:15:36 +0700

    Menambahkan fitur delete

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -83,6 +83,10 @@ class TestimonialController extends Controller
      */
     public function destroy(Testimonial $testimonial)
     {
-        //
+        DB::transaction(function() use ($testimonial) {
+            $testimonial->delete();
+        });
+
+        return redirect()->route('admin.testimonials.index');
     }
 }
