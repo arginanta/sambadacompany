6d1eddd6a2ba5de996b8b19c8a5a78c5b98039b3
Arginanta <arginanta30@gmail.com>
2025-01-22 18:20:48 +0700

    Tampilkan data home page atau Front

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\OurPrinciple;
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
 
 class FrontController extends Controller
 {
     //
     public function index()
     {
-        return view('front.index');
+        $hero_sections = HeroSection::orderByDesc('id')->take(1)->get();
+        $principles = OurPrinciple::take(4)->get();
+        $statistics = CompanyStatistic::take(4)->get();
+        $products = Product::take(3)->get();
+        $teams = OurTeam::take(7)->get();
+        $testimonials = Testimonial::take(5)->get();
+        return view('front.index', compact('hero_sections', 'principles', 'statistics', 'products', 'teams', 'testimonials'));
     }
 }
